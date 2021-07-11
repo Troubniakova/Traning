62331eed2db1e546b5b4005285ba58c690b2efdb
Maria <troubniakova@gmail.com>
2021-07-11 15:48:15 +0300

    if else statements

--- Traning/Program.cs
+++ Traning/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using Traning.second_lesson;
 
 namespace Traning
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private const string Question = "Hello! What is your name?";
+        private const string Question2 = "How old are you?";
+        private const string Greeting = "Hello ";
+
+        private static string Answer;
+        private static string Answer2;
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Question);
+
+            Answer = Console.ReadLine();
+
+            Console.WriteLine(Question2);
+            Answer2 = Console.ReadLine();
+
+            int age = int.Parse(Answer2);
+            if (age > 0 && age < 99)
+                {
+                if (age < 7)
+                {
+                    Console.WriteLine(Greeting + Answer + "Pass is restricted");
+                }
+                else if (int.Parse(Answer2) < 12)
+                {
+                    Console.WriteLine(Greeting + Answer + "Please, go. You have a discount");
+
+                }
+                else
+                {
+                    Console.WriteLine(Greeting + Answer + "Please, go and enjoy");
+                }
+
+                
+
+                Console.ReadLine();
+
+            }
         }
+             
     }
 }
---
+++ Traning/second_lesson/BasicCsharrp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace Traning.second_lesson
+{
+    public class BasicCsharrp1
+    {
+        public static void BasicCsharrpRun()
+        {
+            string message;
+
+            message = " Have a nice day";
+
+            string message2;
+
+            message2 = "Hello world";
+
+            Console.WriteLine("Hello World!" + message);
+
+            message = " Starting calculation...";
+
+            Console.WriteLine(message);
+
+            Console.WriteLine(Sum(2, 3));
+
+            Console.ReadLine();
+        }
+        public static int Sum(int value1, int value2)
+        {
+
+            return value1 + value2;
+
+        }
+
+
+    }
+            
+    }
+
